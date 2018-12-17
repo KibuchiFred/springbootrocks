54112a165901b25ad557c2f1a820ccb5dd809d2e
ajkr195 <34718091+ajkr195@users.noreply.github.com>
2018-12-17 15:56:33 -0600

    error fix

--- src/main/java/com/spring/boot/rocks/validator/UserEditValidator.java
+++ src/main/java/com/spring/boot/rocks/validator/UserEditValidator.java
@@ -35,8 +35,6 @@ public class UserEditValidator implements Validator {
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "roles", "NotEmpty");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "roles", "NoRoleSelected");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "programareas", "NotEmpty");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "programareas", "NoProgramareaSelected");
-
+		
 	}
 }
