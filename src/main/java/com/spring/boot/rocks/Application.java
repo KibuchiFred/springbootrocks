60236da4e9bb7dab3b98097f2751f64ec57f4173
ajkr195 <34718091+ajkr195@users.noreply.github.com>
2019-09-08 11:22:20 -0500

    update

--- src/main/java/com/spring/boot/rocks/Application.java
+++ src/main/java/com/spring/boot/rocks/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Spring BootApp
 @SpringBootApplication
-public class MyApp {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyApp.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
